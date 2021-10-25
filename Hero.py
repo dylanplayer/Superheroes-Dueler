cbbce68e18396adb7b6af0d84bf0de04900b0679
Dylan Player <dylanplayer@Dylans-MacBook-Pro.local>
2021-10-25 16:16:40 -0700

    Update Hero

--- Hero.py
+++ Hero.py
@@ -1,22 +1,70 @@
+from os import fsdecode
 import random
+from Ability import Ability
+from Armor import Armor
 
 class Hero:
     def __init__(self, name, starting_health=100):
         self.name = name
         self.starting_health = starting_health
         self.current_health = starting_health
+        self.abilities = []
+        self.armors = []
     
     def fight(self, opponent):
-        total = self.current_health + opponent.current_health
-        p1_chance = self.current_health / total
-        if (random.randint(0,100) < p1_chance * 100):
-            print(f'{self.name} defeats {opponent.name}')
+        if len(self.abilities) == 0 and len(opponent.abilities) == 0:
+            print(f'{self.name} and {opponent.name} draw')
         else:
-            print(f'{opponent.name} defeats {self.name}')
+            while self.current_health > 0 and opponent.current_health > 0:
+                if len(self.abilities) > 0:
+                    total_damage = self.attack()
+                    if len(opponent.armors) > 0:
+                        total_damage -= opponent.block()
+                    opponent.take_damage(total_damage)
+                if len(opponent.abilities) > 0:
+                    total_damage = opponent.attack()
+                    if len(self.armors) > 0:
+                        total_damage -= self.block()
+                    self.take_damage(total_damage)
+            if self.is_alive():
+                print(f'{self.name} Wins')
+            else:
+                print(f'{opponent.name} Wins')
+                
 
+    def add_ability(self, ability):
+        self.abilities.append(ability)
 
-if __name__ == '__main__':
-    hero1 = Hero("Wonder Woman", 300)
-    hero2 = Hero("Dumbledore", 250)
+    def attack(self):
+        total_damage = 0
+        for ability in self.abilities:
+            total_damage += ability.attack()
+        return total_damage
 
+    def add_armor(self, armor):
+        self.armors.append(armor)
+        
+    def defend(self):
+        total_armor = 0
+        for armor in self.armors:
+            total_armor += armor.block()
+        return total_armor
+
+    def take_damage(self, damage):
+        self.current_health -= damage
+
+    def is_alive(self):
+        return self.current_health > 0
+
+if __name__ == "__main__":
+    hero1 = Hero("Wonder Woman")
+    hero2 = Hero("Dumbledore")
+    ability1 = Ability("Super Speed", 300)
+    ability2 = Ability("Super Eyes", 130)
+    ability3 = Ability("Wizard Wand", 80)
+    ability4 = Ability("Wizard Beard", 20)
+    hero1.add_ability(ability1)
+    hero1.add_ability(ability2)
+    hero2.add_ability(ability3)
+    hero2.add_ability(ability4)
     hero1.fight(hero2)
